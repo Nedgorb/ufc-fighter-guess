bca617dee361a46f710680001f644695b1def818
Ayden Brogden <aydenbrogden@MacBookAir.home>
2025-03-31 00:52:22 -0700

    Set up routing with react-router

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,17 @@
-import './index.css'; // ← this must be here
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import FighterOfTheDay from './components/FighterOfTheDay';
+import UnlimitedMode from './components/UnlimitedMode';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Router>
+      <Routes>
+        <Route path="/" element={<FighterOfTheDay />} />
+        <Route path="/unlimited" element={<UnlimitedMode />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
 );
